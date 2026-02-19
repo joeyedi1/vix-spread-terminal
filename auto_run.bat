49629170b835b42b59bf8700877e8931e1812d15
joeyedi1 <joeyedi1@users.noreply.github.com>
2026-02-19 14:48:36 +0800

    minor

--- auto_run.bat
+++ auto_run.bat
@@ -2,11 +2,14 @@
 :: 1. Go to the correct project folder
 cd /d "C:\Users\Admin\Desktop\joe\VIX dashboard"
 
-:: 2. Run the script and capture ALL output to log
+:: 2. Force UTF-8 so emojis don't crash the script
+set PYTHONIOENCODING=utf-8
+
+:: 3. Run the script and capture ALL output to log
 echo ========================================== >> fetch_log.txt
 echo Run started at %date% %time% >> fetch_log.txt
 python vix_data_fetcher.py >> fetch_log.txt 2>&1
 
-:: 3. Log completion
+:: 4. Log completion
 echo Run finished at %date% %time% >> fetch_log.txt
 echo ========================================== >> fetch_log.txt
